16cbfe10f2937aa69342494859e670629367ea77
Cuong <cuong10d4hungvuongk13@gmail.com>
2025-11-22 14:41:02 +0700

    Lay du lieu cho trang danh sach san pham Admin

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,7 +1,15 @@
+const Product = require('../../model/product.model')
+
+
 //[GET] /admin/products
 module.exports.products = async (req, res) => {
 
+    const products = await Product.find({
+        deleted: false
+    })
+
     res.render('admin/pages/products/index', {
-        titlePage: 'Quản lý sản phẩm'
+        titlePage: 'Quản lý sản phẩm',
+        products: products
     })
 }
