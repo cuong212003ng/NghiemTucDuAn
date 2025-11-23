3c528325b87c9e83abde53c220e590a23bc3e10a
Cuong <cuong10d4hungvuongk13@gmail.com>
2025-11-23 15:21:41 +0700

    Viet BE chuc nang loc trang thai san pham

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -3,11 +3,21 @@ const Product = require('../../model/product.model')
 
 //[GET] /admin/products
 module.exports.products = async (req, res) => {
+    //console.log(req.query.status)
 
-    const products = await Product.find({
+    let find ={
         deleted: false
-    })
+    }
 
+    if(req.query.status){
+        find.status = req.query.status
+    }
+    
+    const products = await Product.find(find)
+
+
+
+    
     res.render('admin/pages/products/index', {
         titlePage: 'Quản lý sản phẩm',
         products: products
