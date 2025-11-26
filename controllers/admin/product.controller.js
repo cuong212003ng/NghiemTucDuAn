fdae0c3cce46d35736f047811ccdb4f8af8795de
Cuong <cuong10d4hungvuongk13@gmail.com>
2025-11-26 16:19:10 +0700

    Chuc nang Phan Trang Pagination

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -23,21 +23,40 @@ module.exports.products = async (req, res) => {
 
     if(objectSearch.regex){
         find.title = objectSearch.regex
-    }
-       
+    }      
     //End Search
     
+    //Pagination
+    let objectPagination = {
+        currentPage: 1,
+        limitItems: 5
+    }
+
+    if(req.query.page){
+        objectPagination.currentPage = parseInt(req.query.page)       
+    }
+
+    // Trang hien tai - 1 * so luong san pham tren trang = so luong san pham can bo qua
+    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItems
+    
+    const countProducts = await Product.countDocuments(find)
+    const totalPage = Math.ceil(countProducts / objectPagination.limitItems)
+    objectPagination.totalPage = totalPage
+
+    //End Pagination
 
 
 
  
 
-    const products = await Product.find(find)
+    const products = await Product.find(find).limit(objectPagination.limitItems).
+    skip(objectPagination.skip)
 
     res.render('admin/pages/products/index', {
         titlePage: 'Quản lý',
         products: products,
         filterStatus: filterStatus,
-        keyword: objectSearch.keyword
+        keyword: objectSearch.keyword,
+        pagination: objectPagination
     })
 }
