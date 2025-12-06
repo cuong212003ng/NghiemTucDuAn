17bfa650cafc878bf9bfc56b120e32b895472d79
Cuong <cuong10d4hungvuongk13@gmail.com>
2025-12-06 15:42:20 +0700

    Trang chi tiet san pham client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -17,3 +17,24 @@ module.exports.products = async (req, res) => {
         products: products
     })
 }
+
+module.exports.productDetail = async (req, res) => {
+    
+    let find = {
+        slug: req.params.slug,
+        deleted: false,
+        status: 'active'
+    }
+
+    const product = await Product.findOne(find)
+    
+    if(!product){
+        req.flash('error', 'Sản phẩm không tồn tại')
+        return res.redirect('/products')
+    }
+    
+    res.render('client/pages/products/detail',{
+        titlePage: 'Chi tiết sản phẩm',
+        product: product
+    })
+}
--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -5,4 +5,6 @@ const productController = require('../../controllers/client/product.controller')
 
 router.get('/', productController.products)
 
+router.get('/:slug', productController.productDetail)
+
 module.exports = router
