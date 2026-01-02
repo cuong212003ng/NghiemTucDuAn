8908e6d93909f561b32e316387170abd0f2313be
Cuong <cuong10d4hungvuongk13@gmail.com>
2026-01-02 14:19:57 +0700

    Tao collection roles

---
+++ model/roles.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const rolesSchema = new mongoose.Schema({
+    title:String,
+    description:String,
+    permissions: {
+        type: Array,
+        default: ["create", "read", "update", "delete"]
+    },
+    deleted:{
+        type: Boolean,
+        default: false
+    },
+    deletedAt:Date
+},
+{
+    timestamps: true
+});
+
+
+const Roles = mongoose.model('Roles', rolesSchema, 'roles');
+
+
+module.exports = Roles;
