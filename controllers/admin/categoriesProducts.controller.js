42a51209e5d279fa16cbf40459274b32fca8b569
Cuong <cuong10d4hungvuongk13@gmail.com>
2026-01-02 11:28:13 +0700

    Hoan thanh chuc nang danh muc san pham

--- controllers/admin/categoriesProducts.controller.js
+++ controllers/admin/categoriesProducts.controller.js
@@ -1,18 +1,39 @@
 const ProductCategories = require('../../model/product-categories.model')
 const systemConfig = require('../../config/system')
 
+const createTreeHelper = require('../../helpers/create-tree')
+
 //[GET] /admin/categories/products
 module.exports.categoriesProducts = async (req, res) => {
+    let find = {
+        deleted: false
+    }
+
+    
+    const records = await ProductCategories.find(find)
+
+    const newRecords = createTreeHelper.tree(records)
 
     res.render('admin/pages/categories/index', {
-        titlePage: 'Danh mục sản phẩm'
+        titlePage: 'Danh mục sản phẩm',
+        records: newRecords
     })
 }
 
 //[GET] /admin/categories/products/create
 module.exports.create = async (req, res) => {
+
+    let find = {
+        deleted: false
+    }
+
+    const records = await ProductCategories.find(find)
+
+    const newRecords = createTreeHelper.tree(records)
+
     res.render('admin/pages/categories/create', {
-        titlePage: 'Thêm danh mục sản phẩm'
+        titlePage: 'Thêm danh mục sản phẩm',
+        records: newRecords
     })
 }
 
